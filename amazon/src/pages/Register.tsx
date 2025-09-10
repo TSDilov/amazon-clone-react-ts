682736ede23266349291e7335e4ec97c654bb60e
TSDilov <cv.dilov@abv.bg>
2025-09-10 12:43:13 +0300

    Creating the front-end project and the Register and Sign-In UI

---
+++ amazon/src/pages/Register.tsx
@@ -0,0 +1,10 @@
+import AuthLayout from "../features/auth/components/AuthLayout";
+import RegistrationForm from "../features/auth/components/RegistrationForm";
+
+const RegisterPage = () => {
+    return <AuthLayout>
+        <RegistrationForm/>
+    </AuthLayout>
+};
+
+export default RegisterPage;
---
+++ amazon/src/pages/Signin.tsx
@@ -0,0 +1,10 @@
+import AuthLayout from "../features/auth/components/AuthLayout";
+import SigninForm from "../features/auth/components/SigninForm";
+
+const SigninPage = () => {
+    return <AuthLayout>
+        <SigninForm/>
+    </AuthLayout>
+};
+
+export default SigninPage;
